0daed95d8018640e3f112e38d01dc1f5f5e5d99d
asing44 <adamwarsing@gmail.com>
2024-04-08 01:23:24 -0400

    Project Init

---
+++ scripts.js
@@ -0,0 +1,8 @@
+import { gsap } from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
