392dd290e52ec84ef376f0a66effe8e2e02471f8
asing44 <adamwarsing@gmail.com>
2024-04-15 08:55:55 -0400

    Clear

--- src/scripts.js
+++
@@ -1,8 +0,0 @@
-import { gsap } from "gsap";
-
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-
-
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-
