edd8b6bf079c63d4eccacc7426957c5501e94e3d
asing44 <adamwarsing@gmail.com>
2024-04-16 02:11:14 -0400

    Nav Version 1

---
+++ src/scripts.js
@@ -0,0 +1,15 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+lenis.on("scroll", ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
