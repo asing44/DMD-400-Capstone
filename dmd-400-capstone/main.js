f9006d381a18e37c4efcda764d9ba608253dbe96
asing44 <adamwarsing@gmail.com>
2024-04-19 15:04:24 -0400

    Hero ---- Design finished

--- dmd-400-capstone/main.js
+++ dmd-400-capstone/main.js
@@ -1,5 +1,4 @@
 import './output.css'
-import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import { gsap } from "gsap";
@@ -34,48 +33,88 @@ gsap.ticker.lagSmoothing(0);
  *
  */
 
-const b = new rive.Rive({
-  src: "./rive/nav-button.riv",
-  canvas: document.getElementById("navButton"),
-  autoplay: true,
-  stateMachines: "SM__nav-button",
-  onLoad: () => {
-    b.resizeDrawingSurfaceToCanvas();
-  },
-});
+// const b = new rive.Rive({
+//   src: "./rive/nav-button.riv",
+//   canvas: document.getElementById("navButton"),
+//   autoplay: true,
+//   stateMachines: "SM__nav-button",
+//   onLoad: () => {
+//     b.resizeDrawingSurfaceToCanvas();
+//   },
+// });
 
 // Resize window for animations
-window.addEventListener(
-  "resize",
-  () => {
-    b.resizeDrawingSurfaceToCanvas();
-  },
-  false
-);
+// window.addEventListener(
+//   "resize",
+//   () => {
+//     b.resizeDrawingSurfaceToCanvas();
+//   },
+//   false
+// );
+
+/* 
+Detects whenever screen is moving up or down
+*/
+function screenMoving() {
+  let scroll = 0;
+
+  ScrollTrigger.create({
+    trigger: "#body",
+    start: "top top", // Trigger at the top of the viewport
+    end: "bottom bottom", // Trigger at the bottom of the viewport
+    onUpdate: (self) => {
+      let scrollDirection = self.direction; // 1 for down, -1 for up
+      let currentScrollTop = self.scroll(); // Current scroll position
+
+      if (scrollDirection === 1) {
+        
+      } else if (scrollDirection === -1) {
+        console.log("Scrolling Up");
+      }
+      // Update last scroll position
+      scroll = currentScrollTop;
+    },
+  });
+
+  return scroll
+}
+
 
 // ======================
 // Section: Navigation
 // ======================
 
-let lastScrollTop = 0;
+// Navigation pinning
+let nav = document.getElementsByClassName("nav")[0];
+console.log(nav)
 
-// Setup ScrollTrigger
-ScrollTrigger.create({
-  trigger: "#body",
-  start: "top top", // Trigger at the top of the viewport
-  end: "bottom bottom", // Trigger at the bottom of the viewport
-  onUpdate: (self) => {
-    const scrollDirection = self.direction; // 1 for down, -1 for up
-    let currentScrollTop = self.scroll(); // Current scroll position
-
-    if (scrollDirection === 1) {
-      console.log("Scrolling Down");
-    } else if (scrollDirection === -1) {
-      console.log("Scrolling Up");
-    }
-    // Update last scroll position
-    lastScrollTop = currentScrollTop;
-  },
+let retractNav_anim = gsap
+  .timeline({
+    paused: true,
+  })
+  .to(nav, {
+    yPercent: -100,
+    ease: "power2.inOut",
+    yoyo: true,
 });
 
-// ! Create animation specific classes!
+gsap.to(nav, {
+  scrollTrigger: {
+    trigger: ".main",
+    start: "top top",
+    pin: ".nav",
+    onUpdate: (self) => {
+      let scrollDirection = self.direction; // 1 for down, -1 for up
+      let currentScrollTop = self.scroll(); // Current scroll position
+      if (scrollDirection === -1 && currentScrollTop >= nav.clientHeight) {
+        retractNav_anim.play();
+      } else if (scrollDirection === 1) {
+        retractNav_anim.reverse();
+      } else if (currentScrollTop <= (1.2 * nav.clientHeight)) {
+        retractNav_anim.reverse();
+      }
+
+      scroll = currentScrollTop;
+    },
+  }
+})
